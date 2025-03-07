54e77a82ef00c9a12345047fc8ce15aa217c5aab
João Morais <95549158+WhipDino@users.noreply.github.com>
2025-03-06 22:36:17 -0300

    Add files via upload

---
+++ demo (1)/demo/src/main/java/com/example/demo/model/Produto.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "produtos")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private double preco;
+}
